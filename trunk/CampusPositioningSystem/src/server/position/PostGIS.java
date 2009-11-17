f8c37b65c4fd3dffda906e6fe65f14f8b24930fa
natenate@gmail.com <natenate@gmail.com@1d9fd0a6-c43b-11de-9fd0-4189a285c7bf>
2009-11-17 04:29:18 +0000

    refactor

--- trunk/CampusPositioningSystem/src/server/position/PostGIS.java
+++ trunk/CampusPositioningSystem/src/server/position/PostGIS.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-import util.ConnectionParameters;
+import server.ConnectionParameters;
 
 
 import com.vividsolutions.jts.algorithm.ConvexHull;
